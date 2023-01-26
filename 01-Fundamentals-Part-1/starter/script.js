32a77bcb9d2407438e19e1b71de15d6240aa8007
Simon Prophet <simon.s.prophet@outlook.com>
2023-01-26 19:30:17 +0000

    add vales, variables, data types, let, const and var lecture to  script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,32 @@
 js = 'cool';
-if (js === 'cool') alert('we in there');
+// if (js === 'cool') alert('we in there');
 
+/*
 console.log(34 + 23);
+
+
+// LECTURE: Values and Variables
+let myCountry = 'Saint Lucia';
+let myContinent = 'North America';
+let myCountryPopulation = 150000;
+
+console.log(myCountry);
+console.log(myContinent);
+console.log(myCountryPopulation);
+
+// LECTURE: Data Types
+let isIsland;
+
+console.log(typeof myCountry);
+console.log(typeof myContinent);
+console.log(typeof myCountryPopulation);
+console.log(typeof isIsland);
+*/
+
+// LECTURE: let, const and var
+const myCountry = 'Saint Lucia';
+const myContinent = 'North America';
+let myCountryPopulation = 150000;
+const myLanguage = 'english';
+
+// myCountry = 'UK';
