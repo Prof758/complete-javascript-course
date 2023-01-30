ca37f4923137d0006637699467fd67457d36619d
Simon Prophet <simon.s.prophet@outlook.com>
2023-01-30 08:04:51 +0000

    add fundamental part 2 Functions

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,9 @@
+'use strict';
+/* ********** LECTURE: Functions *********** */
+function describeCountry(country, population, capitalCity) {
+  return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+}
+
+console.log(describeCountry('Spain', 23, 'Real'));
+console.log(describeCountry('England', 33, 'London'));
+console.log(describeCountry('Brazil', 38, 'Football'));
