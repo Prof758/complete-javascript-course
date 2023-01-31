a7b9633d18bdee600a5957ce32779d13269f89ae
Simon Prophet <simon.s.prophet@outlook.com>
2023-01-31 16:57:56 +0000

    add completed coding challenge arrays

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -162,3 +162,19 @@ if (neighbours.includes('Germany')) {
 console.log(neighbours.indexOf('Brim'));
 neighbours[3] = 'Domin';
 console.log(neighbours);
+
+/* ****** Coding Challenge  ****** */
+
+function calcTip(bill) {
+  const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+  return tip;
+}
+
+const bills = [125, 555, 44];
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+console.log(tips);
+
+const total = [tips[0] + bills[0], tips[1] + bills[1], tips[2] + bills[2]];
+console.log(total);
