893d28937b7d2718f8aa39e9c8adfb3ab023056e
Simon Prophet <simon.s.prophet@outlook.com>
2023-02-10 18:16:19 +0000

    add fix with coding challenge

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -221,9 +221,9 @@ const mark = {
   fullName: 'Mark Miller',
   mass: 78,
   height: 1.69,
-  calcBMI: function (mass, height) {
-    this.BMI = this.mass / this.height ** 2;
-    return this.BMI;
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
   },
 };
 
@@ -231,16 +231,21 @@ const john = {
   fullName: 'John Smith',
   mass: 92,
   height: 1.95,
-  calcBMI: function (mass, height) {
-    this.BMI = this.mass / this.height ** 2;
-    return this.BMI;
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
   },
 };
 
+mark.calcBMI();
+john.calcBMI();
+
 const higherBMI =
   mark.BMI > john.BMI
-    ? `${mark.fullName}'s BMI (${mark.BMI}) is higher than ${john.fullName}'s BMI (${john.BMI})`
-    : `${john.fullName}'s BMI (${john.BMI}) is higher than ${mark.fullName}'s BMI (${mark.BMI})`;
+    ? `${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})`
+    : `${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`;
 
 console.log(higherBMI);
-console.log(mark.BMI);
+console.log(mark.bmi);
+console.log(john.bmi);
+console.log(john.calcBMI());
