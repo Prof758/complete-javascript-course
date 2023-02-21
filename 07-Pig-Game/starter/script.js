40a8d95f035630e09013d0ae898c6cea98bfd62b
Simon Prophet <simon.s.prophet@outlook.com>
2023-02-21 06:02:53 +0000

    add switching active player

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -11,7 +13,10 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 // Set starting conditions
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
+
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
@@ -23,8 +28,18 @@ btnRoll.addEventListener('click', function () {
   // display dice
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${diceRoll}.png`;
+  // check if rolled 1
   if (diceRoll !== 1) {
+    // Add dice to current score
     currentScore += diceRoll;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    // switch player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
