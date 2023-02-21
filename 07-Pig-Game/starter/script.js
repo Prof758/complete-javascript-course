96c7820fb2eab37a7e7b6957fae877ec87a18aa0
Simon Prophet <simon.s.prophet@outlook.com>
2023-02-21 05:23:47 +0000

    add rolling dice function

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -3,9 +3,28 @@
 // Selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 // Set starting conditions
+let currentScore = 0;
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+// add dice roll
+btnRoll.addEventListener('click', function () {
+  // generate dice roll
+  const diceRoll = Math.trunc(Math.random() * 6) + 1;
+  // display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${diceRoll}.png`;
+  if (diceRoll !== 1) {
+    currentScore += diceRoll;
+    current0El.textContent = currentScore;
+  }
+});
