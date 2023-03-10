f380ede6d0a9848ac4c10402e9f2d56dbacde461
Simon Prophet <simon.s.prophet@outlook.com>
2023-03-10 20:55:57 +0000

    add default parameters

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+// ------ Default Parameters ------
+console.log(`------ Default Parameters ------`);
+
+const bookings = [];
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 99.99 * numPassengers
+) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('BA4532', 3);
+createBooking('BA4532', 5, 450);
+
+console.log(bookings);
