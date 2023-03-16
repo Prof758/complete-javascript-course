2dc3d71f2c3f401e0938fb083fd11d2d591822f4
Simon Prophet <simon.s.prophet@outlook.com>
2023-03-16 21:00:56 +0000

    add How Passing Arguments Works: Value vs. Reference

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -18,6 +18,41 @@ const createBooking = function (
 };
 
 createBooking('BA4532', 3);
-createBooking('BA4532', 5, 450);
+createBooking('BA5032', 5, 450);
+createBooking('BA5535');
 
 console.log(bookings);
+
+// ----- How Passing Arguments Works: Value vs. Reference -------
+console.log(`--- How Passing Arguments Works: Value vs. Reference ---`);
+
+const flight = 'BA342'; // a primitive type
+const simon = {
+  name: 'Simon Prophet',
+  passport: 7898893342,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'BA999'; // is a different variable copy value from flight
+  passenger.name = `Mr. ${passenger.name}`;
+  if (passenger.passport === 7898893342) {
+    console.log(`You are checked in`);
+  } else console.log(`Sorry! Cannot check in`);
+};
+
+// when you pass objects through a function you are changing the original object.
+
+checkIn(flight, simon);
+console.log(flight);
+console.log(simon);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000000);
+};
+
+newPassport(simon);
+checkIn(flight, simon);
+console.log(simon);
+
+// ----- First-Class and Higher-Order Functions -------
+console.log(`--- First-Class and Higher-Order Functions ---`);
