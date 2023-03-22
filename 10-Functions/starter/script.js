43e79e8d751244ed6f389520df8ac6c041ad7e6a
Simon Prophet <simon.s.prophet@outlook.com>
2023-03-22 22:11:06 +0000

    add complete coding challenge 1

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -105,7 +105,8 @@ const greetArr = greeting => name => console.log(`${greeting} ${name}`);
 
 greetArr('Hi')('Big Prof');
 
-// The call and apply methods
+// -----The call and apply methods------
+
 console.log(`--- The call and apply methods ---`);
 
 // airline object
@@ -142,7 +143,8 @@ const book = airCaribbean.book;
   },
   */
 
-// using the call function methods
+// -----using the call function methods------
+
 // the call method allows us to manually and explicitly set the this keyword to the first argument.
 book.call(airLucia, 758, 'Jev Broomes');
 console.log(airLucia);
@@ -214,3 +216,79 @@ const newTax = function (rate) {
 const newVAT = newTax(0.25);
 console.log(newVAT(150));
 console.log(newVAT(100));
+
+// Coding Challenge #1
+
+/* 
+Let's build a simple poll app!
+
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+
+Here are your tasks:
+
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+  
+  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what should the this keyword look like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+GOOD LUCK 😀
+*/
+
+const poll = {
+  question: ' What is your favorite programming language?',
+  options: ['0: JavaScript', '1:Python', '2:Rust', '3:C++'],
+  // This generates [0, 0, 0, 0]. More in the next section 😃
+  answers: new Array(4).fill(0),
+  registerNewAnswer: function () {
+    const userAnswer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+      )
+    );
+    console.log(userAnswer);
+    /* typeof answer === 'number' &&
+      answer < this.answers.length &&
+      this.answers[answer]++; */
+    if (userAnswer >= 0 && userAnswer <= 3) {
+      this.answers[userAnswer] += 1;
+      //console.log(this.answers);
+    } else console.log(`Select a number between 0 and 3`);
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults: function (type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll results: ${this.answers.join(',')}`);
+    }
+  },
+};
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+const DATA1 = [5, 2, 3];
+const DATA2 = [1, 5, 3, 9, 6, 1];
+
+poll.displayResults.call({ answers: DATA1 }, 'string');
+poll.displayResults.call({ answers: DATA1 });
+poll.displayResults.call({ answers: DATA2 }, 'string');
+poll.displayResults.call({ answers: DATA2 });
