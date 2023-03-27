a9f4ade77afe359823783dc499c5ee0100b48073
Simon Prophet <simon.s.prophet@outlook.com>
2023-03-27 10:09:35 +0100

    add coding basic array methods

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -74,3 +74,54 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+// Array methods
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// SLICE method: extract part of the array and returns a new array. use the slice method when you want to chain multiple methods together.
+
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4)); // 2nd and 3rd index copied 4th not included
+console.log(arr.slice(-2)); // last 2 elements in array
+console.log(arr.slice(-1)); // last element in array
+console.log(arr.slice(1, -2));
+console.log(arr.slice()); // makes a copy of whole array
+console.log([...arr]); // same as above
+
+// SPLICE method changes the original array (mutates it), it is used to remove elements from an array
+let arrSplice = ['a', 'b', 'c', 'd', 'e'];
+arrSplice.splice(-1); // will remove the last element from the arr
+console.log(arrSplice); // output ['a', 'b', 'c', 'd']
+arrSplice.splice(1, 2); // 1 is that start point, 2 is the number of element you want removed
+console.log(arrSplice);
+
+// REVERSE method mutates the original array
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2); // the reverse method has mutated the original array
+
+// CONCAT joins arrays without mutating the original array
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+// JOIN
+console.log(letters.join(' - ')); // returns a string
+
+// at method
+// new method that can be used on array and strings
+
+const arrAt = [24, 45, 76];
+console.log(arrAt[0]);
+console.log(arrAt.at(0));
+
+// to find the last element in array
+console.log(arrAt[arrAt.length - 1]);
+console.log(arrAt.slice(-1)[0]);
+console.log(arrAt.at(-1)); // can be use to chain multiple methods together.
+
+// AT method used with springs
+console.log('simon'.at(0));
+console.log('simon'.at(-1));
