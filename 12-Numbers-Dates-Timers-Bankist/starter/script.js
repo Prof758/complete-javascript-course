4e18c220c000cbde3ed7948eef4c54409e0b04ec
Simon Prophet <simon.s.prophet@outlook.com>
2023-05-22 16:26:16 +0100

    add Math and rounding lesson

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -288,3 +288,45 @@ console.log(Number.isFinite(23 / 0)); //NaN
 console.log(Number.isInteger(23));
 console.log(Number.isInteger(23.0));
 console.log(Number.isInteger(23 / 0)); // NaN
+
+// Math and Rounding
+
+console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2)); // find square root
+console.log(8 ** (1 / 3)); // find cube
+
+console.log(Math.max(5, 18, 23, 11, 2)); // 23
+console.log(Math.max(5, 18, '23', 11, 2)); // 23
+console.log(Math.max(5, 18, '23px', 11, 2)); // NaN
+
+console.log(Math.min(5, 18, 23, 11, 2)); // 2
+
+console.log(Math.PI * Number.parseFloat('10px') ** 2); // area of circle
+
+console.log(Math.trunc(Math.random() * 6) + 1); // random num 0 - 6
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min) + 1) + min;
+// 0...1 -> 0...(max - min) -> min...max
+console.log(randomInt(10, 20));
+
+// Rounding integers
+console.log(Math.round(23.3)); // rounds to the nearest int
+console.log(Math.round(23.9));
+
+console.log(Math.ceil(23.3)); // always rounds up
+console.log(Math.ceil(23.9));
+
+console.log(Math.floor(23.3)); // always rounds down
+console.log(Math.floor('23.9'));
+
+console.log(Math.trunc(23.3)); // removes decimal point
+
+console.log(Math.trunc(-23.3)); // -23
+console.log(Math.floor(-23.3)); // -24
+
+// Rounding decimals
+console.log((2.7).toFixed(0)); // returns a string
+console.log((2.7).toFixed(3));
+console.log((2.345).toFixed(2));
+console.log(+(2.345).toFixed(2));
