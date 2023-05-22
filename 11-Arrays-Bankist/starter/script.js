2e63558fea9d906970041ea0f68dec253dd5ea61
Simon Prophet <simon.s.prophet@outlook.com>
2023-05-22 16:25:07 +0100

    add number fix (+) and data. Also fixed display by adding 2 decimal points to all numnber

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -10,6 +10,19 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
 };
 
 const account2 = {
@@ -17,6 +30,19 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const account3 = {
@@ -35,7 +61,7 @@ const account4 = {
 
 const account5 = {
   owner: 'Simon Prophet',
-  movements: [1430, 1000, 1700, 250, 990],
+  movements: [1430, 1000.76, 1700, 250.5, 990.53],
   interestRate: 2.3,
   pin: 5555,
 };
@@ -88,7 +114,7 @@ const displayMovements = function (movements, sort = false) {
       i + 1
     } ${type}</div>
           <div class="movements__date">3 days ago</div>
-          <div class="movements__value">${mov}</div>
+          <div class="movements__value">£${mov.toFixed(2)}</div>
     </div>
     `;
     containerMovements.insertAdjacentHTML('afterbegin', html);
@@ -116,7 +142,7 @@ const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce(function (acc, mov) {
     return acc + mov;
   }, 0);
-  labelBalance.textContent = `£${acc.balance}`;
+  labelBalance.textContent = `£${acc.balance.toFixed(2)}`;
 };
 
 // Display Summary
@@ -125,12 +151,12 @@ const calcDisplaySummary = function (acc) {
   const deposit = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `£${deposit}`;
+  labelSumIn.textContent = `£${deposit.toFixed(2)}`;
 
   const withdrawal = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `£${Math.abs(withdrawal)}`;
+  labelSumOut.textContent = `£${Math.abs(withdrawal).toFixed(2)}`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -166,7 +192,7 @@ btnLogin.addEventListener('click', function (e) {
   currentUser = accounts.find(acc => acc.username === inputLoginUsername.value); // use of .find
   // console.log(currentUser);
 
-  if (currentUser?.pin === Number(inputLoginPin.value)) {
+  if (currentUser?.pin === +inputLoginPin.value) {
     // above ? optional chaining
     //Display UI and welcome text
     labelWelcome.textContent = `Welcome back, ${
@@ -187,7 +213,7 @@ btnLogin.addEventListener('click', function (e) {
 
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = +inputTransferAmount.value;
   const receiverAcc = accounts.find(
     acc => acc.username === inputTransferTo.value
   );
@@ -215,7 +241,7 @@ btnTransfer.addEventListener('click', function (e) {
 
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
-  const loanAmount = Number(inputLoanAmount.value);
+  const loanAmount = Math.floor(inputLoanAmount.value);
 
   if (
     loanAmount > 0 &&
@@ -238,7 +264,7 @@ btnClose.addEventListener('click', function (e) {
     // check user is correct
     currentUser.username === inputCloseUsername.value &&
     // check if pin is correct
-    currentUser.pin === Number(inputClosePin.value)
+    currentUser.pin === +inputClosePin.value
   ) {
     //console.log(`OUT`);
     // to find account idex
