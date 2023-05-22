5e5fffc1ded2a23b651ee6733b6dbf967d56c501
Simon Prophet <simon.s.prophet@outlook.com>
2023-05-22 17:09:03 +0100

    add the remainder operator lesson

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -330,3 +330,31 @@ console.log((2.7).toFixed(0)); // returns a string
 console.log((2.7).toFixed(3));
 console.log((2.345).toFixed(2));
 console.log(+(2.345).toFixed(2));
+
+// The Remainder Operator
+
+console.log(5 % 2); // returns 1
+console.log(5 / 2); // 5 = 2 * 2 + 1
+
+console.log(8 % 3); // returns 2
+console.log(8 / 3); // 8 = 2 * 3 + 2
+
+console.log(6 % 2); // returns 0 therefore 6 is even
+console.log(6 / 2);
+
+console.log(7 % 2); // returns 1 therefore 7 is odd
+console.log(7 / 2);
+
+const isEven = n => n % 2 === 0; // simple function to check for even num
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+    // 0, 2, 4, 6
+    if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+    // 0, 3, 6, 9
+    if (i % 3 === 0) row.style.backgroundColor = 'blue';
+  });
+});
