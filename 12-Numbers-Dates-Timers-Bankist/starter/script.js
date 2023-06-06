3e33122a05c1d23d3dc9196f21b8ab5cac976fec
Simon Prophet <simon.s.prophet@outlook.com>
2023-06-06 21:14:54 +0100

    add creating dates

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -376,3 +376,40 @@ console.log(PI);
 
 console.log(Number('230_000'));
 console.log(parseInt('230_000'));
+
+// Creating Dates
+
+// Create a date
+
+const now = new Date();
+console.log(now); // current date
+
+console.log(new Date('Aug 02 2020 18:05:41'));
+console.log(new Date('December 24, 2015'));
+console.log(new Date(account1.movementsDates[0]));
+
+console.log(new Date(2037, 10, 19, 15, 23, 5));
+console.log(new Date(2037, 10, 31));
+
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
+// Working with dates
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth()); // 0 based 0-Jan 1-Feb ...
+console.log(future.getDate()); // day of the month
+console.log(future.getDay()); // day of the week. 0 based 0-Sun 1-Mon ...
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime()); // get time stamp
+
+console.log(new Date(2142256980000));
+
+console.log(Date.now()); // get current time stamp
+
+future.setFullYear(2040);
+console.log(future);
