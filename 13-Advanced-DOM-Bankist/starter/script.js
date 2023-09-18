85b1f5152d75ee9940117114a0166286334d946e
Simon Prophet <simon.s.prophet@outlook.com>
2023-09-18 22:06:37 +0100

    add completed lesson on Selecting, Creating, and Deleting Elements

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -32,3 +32,52 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+///////////////////////////////////////////
+//Selecting, Creating, and Deleting Elements
+console.log('*** Selecting, Creating, and Deleting Elements ***');
+///////////////////////////////////////////
+
+// Selecting
+console.log(document.documentElement); // selects complete HMTL page
+console.log(document.head); // selects head
+console.log(document.body); // selects body
+
+const header = document.querySelector('.header'); // selects element with class of header
+const allSections = document.querySelectorAll('section');
+console.log(allSections);
+
+console.log(document.getElementById('section--1'));
+console.log(document.getElementsByTagName('button')); //returns an HTML collection
+console.log(document.getElementsByClassName('btn'));
+
+// Creating and inserting elements
+
+// .insertAdjacentHTMl()
+const container = document.getElementById('section--1');
+const htmlToInsert = '<p>This is some new HTML content</p>';
+
+// Insert the HTML as the last child of the container element
+container.insertAdjacentHTML('beforeend', htmlToInsert);
+
+// .createElement()
+const cookieText = document.createElement('div');
+cookieText.classList.add('cookie-message');
+cookieText.innerHTML =
+  " We use cookie to improve your web experience. <button class='btn btn--close--cookie'> Got it!</button>";
+
+header.prepend(cookieText);
+// header.append(cookieText);
+//Create a live element that can only live in one place
+// to create copies
+// header.append(cookieText.cloneNode(true));
+
+header.before(cookieText);
+header.after(cookieText);
+
+// Delete elements
+const cookieBtn = document.querySelector('.btn--close--cookie');
+
+cookieBtn.addEventListener('click', () => {
+  cookieText.remove();
+});
