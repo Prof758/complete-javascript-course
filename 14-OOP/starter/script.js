23eefaac71662aa3db736b9dff98aff6f2729bd8
Simon Prophet <simon.s.prophet@outlook.com>
2023-10-23 21:52:42 +0100

    add Constructor Functions and the new Operator

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+// to create a construction function
+// construction function
+
+const Person = function (firstName, birthYear) {
+  (this.firstName = firstName), (this.birthYear = birthYear);
+};
+
+// to create an object from the construction function
+
+const simon = new Person('Simon', 1982);
+console.log(simon);
+console.log(simon instanceof Person);
+
+// The new operator does 4 steps in the background
+// 1. creates a new entry object {}
+// 2. when the function is called the 'this' key word is set to the new object
+// 3. the {} is linked to prototype
+// 4. function will automatically return the new {}
