df9e4d97878bba8509f765453f59e69f98e76b1a
Simon Prophet <simon.s.prophet@outlook.com>
2023-10-23 22:09:05 +0100

    add prototype, using to add methods and properties to objects

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -18,3 +18,17 @@ console.log(simon instanceof Person);
 // 2. when the function is called the 'this' key word is set to the new object
 // 3. the {} is linked to prototype
 // 4. function will automatically return the new {}
+
+// Prototype
+// used to add methods to new object
+
+Person.prototype.calcAge = function () {
+  console.log(2050 - this.birthYear);
+};
+
+simon.calcAge();
+
+// Can also be used to add properties
+
+Person.prototype.player = 'Power forward';
+console.log(simon.player);
