0fa487fcbf017ae022962f1cdd3ef8b2c79ac1fd
Simon Prophet <simon.s.prophet@outlook.com>
2023-11-27 20:52:48 +0000

    add chaining promises lesson completed

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,17 +4,23 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 const renderCountry = function (data, className = '') {
+  const name = data.name.common;
+  const flag = data.flags.svg;
+  const region = data.region;
+  const language = Object.values(data.languages)[0];
+  const currency = Object.values(data.currencies)[0].name;
+
   const html = `
   <article class="country ${className}">
-    <img class="country__img" src="${data.flag}" />
+    <img class="country__img" src="${flag}" />
     <div class="country__data">
-      <h3 class="country__name">${data.name}</h3>
-      <h4 class="country__region">${data.region}</h4>
+      <h3 class="country__name">${name}</h3>
+      <h4 class="country__region">${region}</h4>
       <p class="country__row"><span>👫</span>${(
         +data.population / 1000000
       ).toFixed(1)} people</p>
-      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+      <p class="country__row"><span>🗣️</span>${language}</p>
+      <p class="country__row"><span>💰</span>${currency}</p>
     </div>
   </article>
   `;
@@ -57,8 +63,13 @@ const renderCountry = function (data, className = '') {
 //   });
 // };
 
+//////////////////////////////////////////////
+
 const getCountryData = function (country) {
-  fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`)
+  // country 1
+
+  //fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`)
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
     // .then(function (response) {
     //   console.log(response);
     //   return response.json();
@@ -68,8 +79,26 @@ const getCountryData = function (country) {
     //   renderCountry(data[0]);
     // });
     .then(response => response.json())
-    .then(data => renderCountry(data[0]));
+    .then(data => {
+      console.log(data);
+      console.log(data[0]);
+
+      renderCountry(data[0]);
+
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+
+      // country 2
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => {
+      // console.log(data);
+      renderCountry(data[0], 'neighbour');
+    });
 };
+
 getCountryData('portugal');
-getCountryData('usa');
-getCountryData('germany');
+// getCountryData('usa');
+// getCountryData('germany');
